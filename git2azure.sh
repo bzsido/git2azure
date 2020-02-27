eb62a8661f4b83cacd0754eb3eb79ca662daab96
Balazs Zsido <balazs.zsido@gmail.com>
2020-02-27 11:27:22 +0100

    made sure the script doesn't clone stuff in the wrong folder

--- git2azure.sh
+++ git2azure.sh
@@ -9,12 +9,13 @@ if cd "$STARTDIR"; then
     while read REPO_URL; do
         
         REPO=$(echo "$REPO_URL" | cut -d'/' -f2 | cut -d'.' -f1)
-        AZ_URL=$(az repos create --project "$PROJECT" --name "$REPO" | jq -r '.sshUrl')
-        
-        git clone --bare "$REPO_URL"
-        cd "$REPO".git
-        git push --mirror "$AZ_URL"
-        cd ..
+        if git clone --bare "$REPO_URL"; then
+            
+            cd "$REPO".git
+            AZ_URL=$(az repos create --project "$PROJECT" --name "$REPO" | jq -r '.sshUrl')
+            git push --mirror "$AZ_URL"
+            cd "$STARTDIR"
+        fi
     
     done <"$REPOLIST"
 
